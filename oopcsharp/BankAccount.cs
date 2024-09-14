0b130345a42b560aa9584d0c7199255dd33e8d60
DanielaOdhis <daniela.odhiambo@students.jkuat.ac.ke>
2024-09-14 14:46:31 +0300

    members and constructors

---
+++ oopcsharp/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopcsharp
+{
+    public class BankAccount
+    {
+        //members of the class (5)
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        //constructor because it has the same name as the class
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+    }
+}
--- oopcsharp/Program.cs
+++ oopcsharp/Program.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace oopcsharp
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+
+        class Program
         {
+            static void Main(string[] args)
+            {
+                var account = new BankAccount("Daniela", 1000);
+                Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            }
         }
-    }
+
 }
